b9ebdeaf2e84465f53bf9b667f7c8dc1b2b001ec
Hilarie <109528810+hnoes@users.noreply.github.com>
2023-12-14 15:55:39 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style.css'; 
+import App from './App'; // Ensure the path is correct
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
